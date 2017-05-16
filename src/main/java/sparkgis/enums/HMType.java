b4c3efc2d0bb63863c74ab7df7f2d64fade8d7d2
Furqan Baig <fbaig@cs.stonybrook.edu>
2017-05-16 17:52:50 -0400

    Coefficient type bug fix - since tileID removed from native output

--- src/main/java/sparkgis/enums/HMType.java
+++ src/main/java/sparkgis/enums/HMType.java
@@ -1,8 +1,8 @@
 package sparkgis.enums;
 
 public enum HMType{
-    JACCARD (3, "jaccard"),
-    DICE (2, "dice"),
+    JACCARD (2, "jaccard"),
+    DICE (1, "dice"),
     TILEDICE (-1, "tile_dice");
 
     public final int value;
