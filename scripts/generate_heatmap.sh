1a892d78eb821cd8b00ce5d52aa090db11ebf6d1
Furqan Baig <fbaig@cs.stonybrook.edu>
2017-05-18 16:29:43 -0400

    merging from master

--- scripts/generate_heatmap.sh
+++ scripts/generate_heatmap.sh
@@ -1,50 +1,37 @@
 #!/bin/bash
-USAGE="sh run_spark_gis.sh '--uid [32-bit id]' '--algos '[csv-algos-list]'' '--caseids '[csv-caseID-list]'' '--metric [jaccard|dice|tile_dice]' '--input [hdfs|mongodb]' '--output [hdfs|mongodb|client]' '--result_exe_id exe-id'"
+#
+# Generate HeatMap for BMI Data
+# The data needs to be orgnaised as follows on HDFS
+#    * Specify the data & results directories in ../conf/sparkgis.properties
+#        * hdfs-algo-data=/heatmps/data/
+#        * hdfs-hm-results=/heatmaps/reults/
+#    * Each directory in the 'hdfs-algo-data' corresponds to an algorithm
+#        * /heatmaps/data/algorithm-v1/
+#        * /heatmaps/data/algorithm-v2/
+#        * /heatmaps/data/algorithm-v3/ and so on
+#    * Each file in algorithms directory corresponds to a caseID file having spatial information (e.g. id TAB POLYGON)
+#        * /heatmaps/data/algorithm-v1/case-id1
+#        * /heatmaps/data/algorithm-v1/case-id2
+#        * /heatmaps/data/algorithm-v1/case-id3 and so on
+#    * To generate heatmap for caseids 'c1234' and 'c6789' with algorithms 'algorithm-v1' and 'algorithm-v2' the following HDFS datapaths must be valid
+#        * /heatmaps/data/algorithm-v1/c1234
+#        * /heatmaps/data/algorithm-v1/c6789
+#        * /heatmaps/data/algorithm-v2/c1234
+#        * /heatmaps/data/algorithm-v2/c6789
 
-# python code to get caseids from list
-CASEIDCOUNT=1
-caseids=`python - $CASEIDCOUNT <<END
-if __name__ == '__main__':
-    import sys
-    count = sys.argv[1]
-    with open('/home/fbaig/caseids.list') as f:
-        caseids = f.readlines()
-    caseids = [s.split(',')[0] for s in caseids]
-    #caseids = [s.rstrip() for s in caseids]
-        
-    caseids = caseids[:int(count)]
-    #print caseids
-    print ('%s') % ','.join(caseids)
-END`
-
-# hardcoded values for debugging purposes
-className=driver.SparkGISDriver
-jar=../target/uber-sparkgis-1.0.jar
-# command line arguments
-uid='--uid 123456'
-algos=' --algos "yi-algorithm-v1,yi-algorithm-v11"'
-# caseIDs=' --caseids '$caseids
-# algos=' --algos "algo1,algo2"'
-# small
-caseIDs=' --caseids "TCGA-06-1802-01Z-00-DX1"'
-# bigger
-# caseIDs=' --caseids "TCGA-02-0003-01Z-00-DX2"'
-heatmapType=' --metric jaccard'
-# input=' --input hdfs'
-# output=' --output client'
-# # mongodb specific params
-# inputdb='--inputdb u24_segmentation'
-# outputdb='' #'--outputdb temp_db'
-# inputcollection='--inputcollection results'
-# outputcollection='' #'--outputcollection temp_col'
-
-# resultExeID='--result_exe_id fbaig'
+ALGOS=yi-algorithm-v1,yi-algorithm-v11
+CASEIDS=TCGA-06-1802-01Z-00-DX1
+METRIC=jaccard
+TILESIZE=64
 
 # run spark job
-echo "Executing command $SPARK_HOME/bin/spark-submit --class $className $jar $algos $caseIDs $heatmapType"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+LIB_DIR="$SCRIPT_DIR/../lib"
+CLASSNAME=driver.SparkGISBMIHeatMap
+JAR=$SCRIPT_DIR/../target/uber-sparkgis-1.0.jar
 
-$SPARK_HOME/bin/spark-submit --class $className \
-    --conf "spark.driver.extraLibraryPath=/home/fbaig/sbu-bmi-u24/spark-gis/lib" \
-    --conf "spark.executor.extraLibraryPath=/home/fbaig/sbu-bmi-u24/spark-gis/lib" \
-    --conf "spark.default.parallelism=96" \
-    $jar $algos $caseIDs $heatmapType
+$SPARK_HOME/bin/spark-submit \
+    --class $CLASSNAME \
+    --conf "spark.driver.extraLibraryPath=$LIB_DIR" \
+    --conf "spark.executor.extraLibraryPath=$LIB_DIR" \
+    $JAR --algos $ALGOS --caseids $CASEIDS --metric $METRIC --tilesize $TILESIZE
